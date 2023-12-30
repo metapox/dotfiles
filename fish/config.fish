c79e9bb958c246fb8e701ea3dfbc6c199181cda8
metapox <84276752+metapox@users.noreply.github.com>
2023-12-30 23:06:33 +0900

    add fish conig

---
+++ fish/config.fish
@@ -0,0 +1,8 @@
+export PATH="/usr/local/opt/openssl@1.1/bin:$PATH"
+
+export PATH=$PATH:/usr/local/go/bin
+export GOPATH=$HOME/.go
+export PATH=$PATH:$GOPATH/bin
+
+# ailias
+alias dc='docker compose'
