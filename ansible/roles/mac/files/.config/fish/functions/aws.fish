a53a89962b4e2dc29b46e1c01dce88ec52846c8e
metapox <84276752+metapox@users.noreply.github.com>
2024-10-06 13:42:11 +0900

    start instance

--- ansible/roles/mac/files/.config/fish/functions/aws.fish
+++ ansible/roles/mac/files/.config/fish/functions/aws.fish
@@ -19,3 +19,15 @@ function get-instance-id
 
     echo $instance_id
 end
+
+function start-instance
+    set -l instance_id ""
+
+    set instance_id $argv[(math $OPTIND + 1)]
+    if test -z $instance_id
+        echo "Usage: start-instance <instance_id>"echo "Usage: start-instance <instance_id>"
+        return 1
+    end
+
+    aws ec2 start-instances --instance-ids $instance_id
+end
